f68cd93aaff012691ad514cc60a4c979cde23f73
Yeisson19 <yeissoncolmenarez@gmail.com>
2023-08-15 17:13:42 -0400

    guardado 5pm

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const CargandoPeli = async () =>{
 
 	try{
-		const api = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=e1d864e178ea9e978af97c6a0d6b2acb&language=es-ES&page=${pagina}`);
+		const api = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=e1d864e178ea9e978af97c6a0d6b2acb&language=es-ES`);
 		console.log(api);
+
+		if(api.status == 200){
+
+		}
+		else{
+			console.log("Hay un error de coneccion");
+		}
 	}
 	catch{
 
@@ -10,3 +17,5 @@ const CargandoPeli = async () =>{
 	 
 
 }
+
+CargandoPeli();
