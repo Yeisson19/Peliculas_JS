aacdd3ac3cba119bd5cd0c91f7307dbc17cec672
Yeisson19 <yeissoncolmenarez@gmail.com>
2023-08-16 15:44:29 -0400

    coneccion terminada

--- app.js
+++ app.js
@@ -3,15 +3,36 @@ const CargandoPeli = async () =>{
 	//Tratando
 	try{
 		 		//Promesa
-		const api = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=e1d864e178ea9e978af97c6a0d6b2acb&language=es-ES`);
+		const api = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=e1d864e178ea9e978af97c6a0d6b2acb&language=es-ES&append_to_response=videos,images`);
 		console.log(api);
 
 		if(api.status == 200){
+			
+			//opteniendo json
+			const datos = await api.json();
+			//imp datos
+			console.log(datos.results);
+			
+			
+			var vista="";
+			datos.results.forEach(peli => {
+				 vista += `
+					<div class="pelicula">
+						<img class="poster" src="https://image.tmdb.org/t/p/w500/${peli.poster_path}">
+						<h3 class="titulo">${peli.title}</h3>
+					</div>
+				`;
+			});
+
+
+			const contenedor = document.getElementById('contenedor').innerHTML = vista;
 			console.log("exitosa coneccion");
 		}
 		else{
 			console.log("Hay un error de coneccion");
 		}
+
+	
 	}
 	//Atrapar
 	catch(error){
