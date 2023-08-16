7de682da65aebe5d08c2fd57b19d344c8f4f5721
Yeisson19 <yeissoncolmenarez@gmail.com>
2023-08-16 13:59:53 -0400

    dasd

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const CargandoPeli = async () =>{
 	try{
 		const api = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=e1d864e178ea9e978af97c6a0d6b2acb&language=es-ES`);
 		console.log(api);
-
+		//estado coneccion
 		if(api.status == 200){
 
 		}
