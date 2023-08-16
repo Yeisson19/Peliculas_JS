fa636c47a4dc9a02d3292965cd8cd949349a879d
Yeisson19 <yeissoncolmenarez@gmail.com>
2023-08-16 14:17:06 -0400

    cambio en trabajo

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
+//Cargando API -- Funcion Asincrona
 const CargandoPeli = async () =>{
-
+	//Tratando
 	try{
+		 		//Promesa
 		const api = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=e1d864e178ea9e978af97c6a0d6b2acb&language=es-ES`);
 		console.log(api);
-		//estado coneccion
-		if(api.status == 200){
 
+		if(api.status == 200){
+			console.log("exitosa coneccion");
 		}
 		else{
 			console.log("Hay un error de coneccion");
 		}
 	}
-	catch{
-
+	//Atrapar
+	catch(error){
+			console.log(error);
 	}
 	 
 
 }
 
+//callfuntion
 CargandoPeli();
