16ceacc1015949960572a89860dde0a549b56dac
Yeisson19 <yeissoncolmenarez@gmail.com>
2023-08-24 17:16:42 -0400

    Trabajo 24/08 5pm

--- peli.js
+++ peli.js
@@ -85,47 +85,76 @@ function info(i){
 //--------------------------------------
   //pag -> paginas q mostrara
 var pag=1;
-     //ub -> ub de la pagina 
-var  ub=0;
-var final;
 
 $(document).ready(nav);
-
 function nav(){
+//ub -> ub de la pagina 
+  let ub=[];
+  ub.length = 0;
+
+   for(let i=0;i<10;i++){
+	
+	if(pag==1 && i==0){
+		ub[i]=pag;//1
+	}
+	else{
+	ub[i]=Number(pag)+Number(i);
+	}
+	
+	console.log("ub=" + ub[i]);
+   }
 	$('#pagination').html(`
 
 	<nav aria-label="nav-movi">
 	<ul class="pagination justify-content-center">
-	  <li class="page-item"><a class="page-link" onclick="Anterior()" href="#">Previous</a></li>
-	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${Number(ub)+Number(1)}">${Number(ub)+Number(1)}</a></li>
-	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${Number(ub)+Number(2)}">${Number(ub)+Number(2)}</a></li> 
-	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${Number(ub)+Number(3)}">${Number(ub)+Number(3)}</a></li> 
-	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${Number(ub)+Number(4)}">${Number(ub)+Number(4)}</a></li> 
-	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${Number(ub)+Number(5)}">${Number(ub)+Number(5)}</a></li> 
-	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${Number(ub)+Number(6)}">${Number(ub)+Number(6)}</a></li> 
-	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${Number(ub)+Number(7)}">${Number(ub)+Number(7)}</a></li> 
-	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${Number(ub)+Number(8)}">${Number(ub)+Number(8)}</a></li> 
-	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${Number(ub)+Number(9)}">${Number(ub)+Number(9)}</a></li> 
-	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${Number(ub)+Number(10)}">${Number(ub)+Number(10)}</a></li> 
+	  <li class="page-item " id="Anterior"><a class="page-link" onclick="Anterior()" href="#" tabindex="-1">Previous</a></li>
+	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${ub[0]}">${ub[0]}</a></li>
+	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${ub[1]}">${ub[1]}</a></li> 
+	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${ub[2]}">${ub[2]}</a></li> 
+	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${ub[3]}">${ub[3]}</a></li> 
+	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${ub[4]}">${ub[4]}</a></li> 
+	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${ub[5]}">${ub[5]}</a></li> 
+	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${ub[6]}">${ub[6]}</a></li> 
+	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${ub[7]}">${ub[7]}</a></li> 
+	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${ub[8]}">${ub[8]}</a></li> 
+	  <li class="page-item"><a class="page-link" href="#" onclick="pagination(this.id)" id="${ub[9]}">${ub[9]}</a></li> 
 	  <li class="page-item"><a class="page-link" onclick="Siguiente()" href="#">Next</a></li>
 	</ul>
 	</nav>
 	
 	`);
+
 }
+
+document.body.addEventListener('load', function() {
+    console.log('La página ha terminado de cargarse!!');
+});
+
+// document.body
+// function off(){
+
+// if(ub=1){
+
+// 	const id_Anterior = document.getElementById('Anterior');
+// 	console.log(id_Anterior);
+// 	const nuevaCaja = document.createElement(id_Anterior);
+// 	console.log(nuevaCaja);
+
+// 	}
+// }
 
 
 function pagination(n){
+
+	pag = n;
+    var final;
 	console.log(n);
 	// console.log(ub);
-	pag = n;
 
 	if (pag == 10 || pag == 20 || pag == 30 || pag == 40 || pag == 50 || pag == 60 || pag == 70 || pag == 80 || pag == 90) {
-		//cambia los valores de paginacion
-		ub=pag;
 		//N° final de la paginacion
-		final=Number(ub)+Number(10);
-		console.log(ub);
+		final=Number(pag)+Number(10);
+		// console.log(ub);
 		console.log(final);
 		nav();
 	}
